a52228c26b51bc90dffc0e52b09eefd470ab8be4
tkdqjaos999 <tkdqjaos999@gmail.com>
2019-11-24 22:27:14 +0900

    RootPage LoginPage TabPage 설계 밑 탭페이지 초기 구현

---
+++ lib/root_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:insta_flutter_app/tab_page.dart';
+
+class RootPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TabPage();
+  }
+}
